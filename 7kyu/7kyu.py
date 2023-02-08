8630a1e5f8af0d6b9cf7a84a86b69d1e6425ac5d
Mikhail Tikhomirov <mtikhomirov75@gmail.com>
2023-02-08 22:38:01 +0300

    Update 7 kyu

--- 7kyu/7kyu.py
+++ 7kyu/7kyu.py
@@ -237,3 +237,41 @@ def max_df(a_n: int) -> int:
 ​    """
     n = (isqrt(8 * a_n) - 1) // 2
     return n    
+
+
+def can_escape(walls):
+    """ Hurry up, the walls are closing in!.
+        https://www.codewars.com/kata/63ab271e96a48e000e577442
+
+        Task
+        "Given a list walls closing in at you, can you make it past those walls without being hit?"
+        Input
+        walls: an array of 2-ples, each 2-ple storing non-negative numbers representing the distance
+        of the walls closing in at you from each side respectively to the center of the room.
+        Output
+        return a boolean indicating whether it is possible to run past the walls without being hit.
+        Specification
+        You are at the left side in the center of the room
+        You want to reach the right side in the center of the room
+        You can only move in a straight line across the room
+        Walls are closing in at you from both sides of the room
+        Walls stop once they hit the center of the room
+        You and the walls move at the same speed
+        You get hit by a wall if you are on the same tile as a wall
+        If a wall reaches a tile ahead of you, you cannot make it past that wall anymore
+        Input Constraints
+        50 tests with 1 <= number of walls <= 4
+        50 tests with 5 <= number of walls <= 15
+        50 tests with 16 <= number of walls <= 50
+        50 tests with 51 <= number of walls <= 100
+        0 <= starting distance of walls to center of room <= 120
+        Examples
+        In the examples below, you start on tile A and want to reach tile B.
+        You have to walk across the center of the room -.
+        Walls W are closing in on you from the north and south.
+    """
+
+    for i in range(len(walls)):
+        if min(walls[i]) <= i+1:
+            return False
+    return  True
